0b43ced0a3786dc85077873c9ad94877c024f250
Alexandre Jacquin <alexandre.jacquin@kisio.com>
2019-08-13 08:39:38 +0200

    add benches

--- main.go
+++ main.go
@@ -31,14 +31,17 @@ func directPath(k kraken.Kraken, from, to string, quiet bool) {
 	}
 	if !quiet {
 		fmt.Print(j)
+	} else {
+		fmt.Print(".")
 	}
 }
 
-func matrix(k kraken.Kraken, from, to []string, quiet bool) {
+func matrix(k kraken.Kraken, from, to []string, maxDuration int32, quiet bool) {
 	matrix := kraken.StreetNetworkMatrixBuilder{
-		From:   from,
-		To:     to,
-		Kraken: k,
+		From:        from,
+		To:          to,
+		Kraken:      k,
+		MaxDuration: maxDuration,
 	}
 	r, err := matrix.Get()
 	if err != nil {
@@ -51,6 +54,8 @@ func matrix(k kraken.Kraken, from, to []string, quiet bool) {
 	}
 	if !quiet {
 		fmt.Print(j)
+	} else {
+		fmt.Print(".")
 	}
 }
 
@@ -74,10 +79,25 @@ func LoadCoordFromFile(path string) ([]string, error) {
 	return result, nil
 }
 
+func benchmark(duration time.Duration, concurrency int, f func()) {
+	loop := func() {
+		for {
+			f()
+		}
+	}
+	for i := 0; i < concurrency; i++ {
+		go loop()
+	}
+	<-time.After(duration)
+}
+
 func main() {
 	var target string
 	var quiet bool
 	var timeout time.Duration
+	var bench time.Duration
+	var goroutines int
+	var maxDuration int32
 
 	var cmdDP = &cobra.Command{
 		Use:   "directpath <from> <to>",
@@ -86,7 +106,14 @@ func main() {
 		Args:  cobra.MinimumNArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			k := kraken.NewKrakenZMQ("test", target, timeout)
-			directPath(k, args[0], args[1], quiet)
+			f := func() {
+				directPath(k, args[0], args[1], quiet)
+			}
+			if bench.Seconds() < 1 {
+				f()
+			} else {
+				benchmark(bench, goroutines, f)
+			}
 		},
 	}
 
@@ -113,18 +140,26 @@ func main() {
 					logrus.Fatal(err)
 				}
 			}
-			logrus.Info(from)
-			logrus.Info(to)
-			matrix(k, from, to, quiet)
+			f := func() {
+				matrix(k, from, to, maxDuration, quiet)
+			}
+			if bench.Seconds() < 1 {
+				f()
+			} else {
+				benchmark(bench, goroutines, f)
+			}
 		},
 	}
 	cmdMatrix.Flags().StringVar(&fromFile, "from", "", "file to read from coordinates")
 	cmdMatrix.Flags().StringVar(&toFile, "to", "", "file to read to coordinates")
+	cmdMatrix.Flags().Int32Var(&maxDuration, "max-duration", 30*60, "max duration to explore")
 
 	rootCmd := &cobra.Command{}
 	rootCmd.PersistentFlags().StringVarP(&target, "target", "t", "tcp://127.0.0.1:6000", "kraken to target")
 	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "remove normal output")
 	rootCmd.PersistentFlags().DurationVarP(&timeout, "timeout", "d", 10*time.Second, "kraken timeout")
+	rootCmd.PersistentFlags().DurationVar(&bench, "bench", 0*time.Second, "run the benchmark for the specified duration")
+	rootCmd.PersistentFlags().IntVarP(&goroutines, "concurrency", "c", 1, "number of goroutine to launch in bench modes")
 	rootCmd.AddCommand(cmdDP)
 	rootCmd.AddCommand(cmdMatrix)
 	_ = rootCmd.Execute()
